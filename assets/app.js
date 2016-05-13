97ca64fb0eee2edb2d3dd87fce1823ff7a171a03
syon <syon.xiii@gmail.com>
2016-05-13 20:54:36 +0900

    highlight selected menu

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,9 @@
 $(function() {
+
   $('#article p > img').each(function(){
     $(this).parent().addClass('imgbox');
   });
+
+  $('#gMenu .menu-link[href="'+location.pathname+'"]').closest('li').addClass('selected');
+
 });
