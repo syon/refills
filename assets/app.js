bec0af6836dc9b7866b4b537285cd4e24aeabfff
syon <syon.xiii@gmail.com>
2016-05-13 21:24:25 +0900

    Side menu fixed

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,18 @@ $(function() {
 
   $('#gMenu .menu-link[href="'+location.pathname+'"]').closest('li').addClass('selected');
 
+  var queue = null, wait = 300;
+  doResizeProcess();
+
+  window.addEventListener('resize', function() {
+    clearTimeout( queue );
+    queue = setTimeout(function() {
+      doResizeProcess();
+    }, wait);
+  }, false);
+
+  function doResizeProcess() {
+    $('#gSide').height($(window).height());
+  }
+
 });
