c4d838b865479ce442b7d4c77c60ba2e229252a1
syon <syon.xiii@gmail.com>
2016-06-12 18:19:57 +0900

    :fire: remove menu process

--- assets/app.js
+++ assets/app.js
@@ -4,32 +4,4 @@ $(function() {
     $(this).parent().addClass('imgbox');
   });
 
-  $('#gMenu .menu-link[href="'+location.pathname+'"]').closest('li').addClass('selected');
-
-  resizeProcess();
-
-  menuInitialScroll();
-
 });
-
-function menuInitialScroll() {
-  var offset = $('.selected').closest('.binder').offset().top - 100;
-  $('#gSide').scrollTop(offset);
-}
-
-function resizeProcess() {
-  var queue = null, wait = 300;
-  doResizeProcess();
-
-  window.addEventListener('resize', function() {
-    clearTimeout( queue );
-    queue = setTimeout(function() {
-      doResizeProcess();
-    }, wait);
-  }, false);
-
-  function doResizeProcess() {
-    $('#gSide').height($(window).height());
-    $('#gSide > #gMenu').css('paddingBottom', $(window).height() - 30);
-  }
-}
